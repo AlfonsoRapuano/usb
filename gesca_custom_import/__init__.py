25866135b011a1e38a81b3694b3e81ae27bdbdb0
EdoardoRagaglini <edoardo.ragaglini@gesca.it>
2022-10-26 11:29:12 +0200

    [ADD] added gesca_custom_import

---
+++ gesca_custom_import/__init__.py
@@ -0,0 +1,2 @@
+# -*- coding: utf-8 -*-
+#from . import sh_import_inv
---
+++ gesca_custom_import/__manifest__.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Gesca Custom Import",
+
+    'summary': """
+        Implementazione del modulo sh_all_in_one_basic_import""",
+
+    'description': """
+        Implementazione del modulo sh_all_in_one_basic_import""",
+
+    'author': "Gesca",
+    'website': "https://gesca.it",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    "category": "Extra Tools",
+    'version': '14.0.0.1',
+
+    # any module necessary for this one to work correctly
+    #'depends': ['base', 'sh_all_in_one_basic_import'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        #'views/views.xml',
+        #'views/templates.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        #'demo/demo.xml',
+    ],
+}
